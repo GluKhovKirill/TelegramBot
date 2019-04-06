afe42bef0bcfdbe03fb6a1134ce5a19d2ee29965
OFF LUCK <lyna135@bk.ru>
2019-04-06 23:33:40 +0300

    Update keys.py

--- keys.py
+++ keys.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+
+
 from json import loads
 
 
@@ -9,15 +11,16 @@ try:
         keys = (True, loads(file.read()))
 except BaseException as err:
     keys = (False, err)
-
-
+    
+    
 def get_key(keyname):
     if keys[0]:
         return (True, keys[1][keyname])
     else:
         return keys
-
-
-if __name__ == "__main__":
+    
+    
+if(__name__ == "__main__):
     print("Available keys:")
-    for key in keys[1].keys(): print("KEYNAME:", key)
+    for key in keys[1].keys():
+        print("KEYNAME:", key)
