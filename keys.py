74dd722c372ae1e66f2f03de25b729fcda7fa404
Glu(k)hov Kirill <kirill.gluhov2003@yandex.ru>
2019-04-06 22:02:18 +0300

    Файл для работы с токенами (ключами)

---
+++ keys.py
@@ -0,0 +1,21 @@
+from json import loads
+
+
+try:
+    filename = "keys.keys"
+    with open(filename) as file:
+        keys = (True, loads(file.read()))
+except BaseException as err:
+    keys = (False, err)
+
+
+def get_key(keyname):
+    if keys[0]:
+        return (True, keys[1][keyname])
+    else:
+        return keys
+
+
+if __name__ == "__main__":
+    print("Available keys:")
+    for key in keys[1].keys(): print("KEYNAME:", key)
