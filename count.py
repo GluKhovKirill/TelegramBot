10571c5407d3476806581b5bba546f3a0a2da60c
OFF LUCK <lyna135@bk.ru>
2019-04-09 22:02:24 +0300

    Update count.py

--- count.py
+++ count.py
@@ -109,7 +109,7 @@ class MathExecutor:
         try:
             first = float(first)
             second = float(second)
-            return (True, round(first - second, 8))
+            return (True, Decimal(str(first)) + Decimal(str(second)))
         except ValueError:
             return (False, "NFLT")
         except TypeError:
